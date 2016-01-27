ba3698d34fb83591e6e1257b407a5bd3cf37052f
Denis Saranyuk <denissaranyuk@gmail.com>
2016-01-27 11:05:42 +0200

    social 0.2.1 - geolocation was added

--- public/js/views/profile.js
+++ public/js/views/profile.js
@@ -7,7 +7,7 @@ define(['Backbone', 'Underscore', 'models/user', 'text!templates/profile.html'],
         el: '#mainFrame',
         template: _.template(templateProfile),
         events: {
-
+            'click #location':'getLocation'
         },
 
         initialize: function () {
@@ -23,6 +23,36 @@ define(['Backbone', 'Underscore', 'models/user', 'text!templates/profile.html'],
               alert('profile fetch error');
             }
           });
+        },
+
+        getLocation: function(){
+          var output = this.$el.find('#outLocation');
+
+          if (!navigator.geolocation){
+            output.append("<p>Geolocation is not supported by your browser</p>");
+            return;
+          }
+
+          function success(position) {
+            var latitude  = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            output.append('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>');
+
+            var img = new Image();
+            img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+            output.append(img);
+            this.render();
+          }
+
+          function error() {
+            output.append("Unable to retrieve your location");
+          }
+
+          output.append("<p>Locating…</p>");
+
+          navigator.geolocation.getCurrentPosition(success, error);
         },
 
         render: function () {
